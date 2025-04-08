4e58f7830cfb7c50d32456120ac132fdf9b35299
zeyadAbdo22 <119048836+zeyadAbdo22@users.noreply.github.com>
2025-04-08 05:52:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from PIL import Image
 import io
 import os
 import kagglehub
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 app = FastAPI()
 
 # CORS settings
@@ -57,13 +56,13 @@ def make_prediction(model, img_array, class_labels, binary=False):
     return label, confidence, prediction.tolist()
 
 # ---------- Model Loaders ----------
-
+'''
 def load_lung_model():
     global lung_model
     path = kagglehub.model_download("zeyadabdo/lung-cancer-resnet/keras/v1")
     lung_model_path = os.path.join(path, "lung-cancer-resnet-model.h5")
     lung_model = load_model(lung_model_path, compile=False) 
-
+'''
 def load_brain_model():
     global brain_model
     path = kagglehub.model_download("khalednabawi/brain-tumor-cnn/keras/v1")
@@ -78,11 +77,7 @@ async def load_models():
         print("✅ Brain tumor model loaded.")
     except Exception as e:
         print(f"❌ Failed to load Brain tumor model: {e}")
-    try:
-        load_lung_model()
-        print("✅ lung cancer model loaded.")
-    except Exception as e:
-        print(f"❌ Failed to load lung cancer model: {e}")
+
 
 
 # ---------- Routes ----------
